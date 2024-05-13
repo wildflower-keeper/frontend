51da39c29ad0097a120da6b4f886c161b69f4131
imfreeman1 <dkfrhkt@gmail.com>
2024-05-13 19:23:56 +0900

    Next.js 프로젝트 생성

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "@/styles/globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "들꽃지기 어드민",
+  description: "관리자 페이지",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="kr">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
