f651d25cad9b522b9c7d2f2447b7b2f7d9aa568e
imfreeman1 <dkfrhkt@gmail.com>
2024-05-16 18:43:48 +0900

    인풋 컴포넌트 생성 및 타입 지정

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import React from "react";
+import { InputHTMLAttributes } from "react";
+
+type InputProps = InputHTMLAttributes<HTMLInputElement>;
+
+const Input = ({
+  className,
+  placeholder,
+  onChange,
+  type = "text",
+  value,
+  ...props
+}: InputProps) => {
+  return (
+    <input
+      className={className}
+      placeholder={placeholder}
+      type={type}
+      value={value}
+      onChange={onChange}
+      {...props}
+    />
+  );
+};
+
+export default Input;
