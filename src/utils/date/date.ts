49acb41d7d388f85a45a9245443dc043e9237c53
imfreeman1 <dkfrhkt@gmail.com>
2024-06-28 23:27:33 +0900

    데이트 타입 지정

--- src/utils/date/date.ts
+++ src/utils/date/date.ts
@@ -3,7 +3,11 @@ const dateInfo = () => {
   return `${today.getMonth() + 1}월 ${today.getDate()}일 (${days[today.getDay()]}) ${String(today.getHours()).padStart(2, "0")}:${String(today.getUTCMinutes()).padStart(2, "0")}`;
 };
 
-const days = {
+interface daysType {
+  [idx: number]: string;
+}
+
+const days: daysType = {
   0: "일",
   1: "월",
   2: "화",
@@ -11,6 +15,6 @@ const days = {
   4: "목",
   5: "금",
   6: "토",
-} as const;
+};
 
 export { dateInfo };
