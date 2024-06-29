e652092d54b599b3bd5983182bd8f8a1a7024318
imfreeman1 <dkfrhkt@gmail.com>
2024-06-29 16:47:44 +0900

    데이트 유틸 추가

--- src/utils/date/date.ts
+++ src/utils/date/date.ts
@@ -17,5 +17,16 @@ const dateInfo = () => {
   return `${today.getMonth() + 1}월 ${today.getDate()}일 (${days[today.getDay()]}) ${String(today.getHours()).padStart(2, "0")}:${String(today.getUTCMinutes()).padStart(2, "0")}`;
 };
 
-// eslint-disable-next-line import/prefer-default-export
-export { dateInfo };
+const formatDateTime = (date: Date, option?: "simple" | undefined) => {
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, "0"); // 월은 0부터 시작하므로 1을 더해줌
+  const day = String(date.getDate()).padStart(2, "0");
+  const hours = String(date.getHours()).padStart(2, "0");
+  const minutes = String(date.getMinutes()).padStart(2, "0");
+  const seconds = String(date.getSeconds()).padStart(2, "0");
+
+  return option
+    ? `${year}-${month}-${day}`
+    : `${year}-${month}-${day} ${hours}%3A${minutes}%3A${seconds}.000000`;
+};
+export { dateInfo, formatDateTime };
