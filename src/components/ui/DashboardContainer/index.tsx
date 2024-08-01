94ec766435a0a964e2a70663272a3093b43c44d5
imfreeman1 <dkfrhkt@gmail.com>
2024-08-01 18:25:32 +0900

    대시보드 컨테이너의 width 변경
     - 맥북 사이즈 1366px로 작성 한 것을 w-full로 변경

--- src/components/ui/DashboardContainer/index.tsx
+++ src/components/ui/DashboardContainer/index.tsx
@@ -5,7 +5,7 @@ import React from "react";
 
 const DashboardContainer = () => {
   return (
-    <div className="flex w-[1366px] min-h-screen m-auto bg-dashboardBackgroundColor">
+    <div className="flex w-full min-h-screen m-auto bg-dashboardBackgroundColor">
       <DashboardSideBar />
       <div className="grow flex-col h-full">
         <DashboardHeader />
