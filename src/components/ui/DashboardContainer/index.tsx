ddf8a400264627fab171de1973139bdb9e845c38
imfreeman1 <96756158+imfreeman1@users.noreply.github.com>
2024-08-19 17:58:25 +0900

    Revert "Suspense태그 적용, constants폴더와 hooks폴더 이동"

--- src/components/ui/DashboardContainer/index.tsx
+++ src/components/ui/DashboardContainer/index.tsx
@@ -1,17 +1,14 @@
 import DashboardBody from "@/components/dashboardItems/DashboardBody";
 import DashboardHeader from "@/components/dashboardItems/DashboardHeader";
 import DashboardSideBar from "@/components/dashboardItems/DashboardSideBar";
-import React, { Suspense } from "react";
+import React from "react";
 
 const DashboardContainer = () => {
   return (
     <div className="flex w-full min-h-screen m-auto bg-dashboardBackgroundColor">
       <DashboardSideBar />
       <div className="grow flex-col h-full">
-        <Suspense fallback={<div>loading</div>}>
-          <DashboardHeader />
-        </Suspense>
-
+        <DashboardHeader />
         <DashboardBody />
       </div>
     </div>
