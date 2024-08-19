99bb0cfa283d39a3cdadb9bc82fc97d45a19fa39
imfreeman1 <dkfrhkt@gmail.com>
2024-08-19 17:25:03 +0900

    suspense태그 적용
     - useSearchParams를 사용시 Suspense태그를 사용하지 않으면 error가 발생하는 상황을 해결하기 위해 Suspense태그 사용

--- src/components/ui/DashboardContainer/index.tsx
+++ src/components/ui/DashboardContainer/index.tsx
@@ -1,14 +1,17 @@
 import DashboardBody from "@/components/dashboardItems/DashboardBody";
 import DashboardHeader from "@/components/dashboardItems/DashboardHeader";
 import DashboardSideBar from "@/components/dashboardItems/DashboardSideBar";
-import React from "react";
+import React, { Suspense } from "react";
 
 const DashboardContainer = () => {
   return (
     <div className="flex w-full min-h-screen m-auto bg-dashboardBackgroundColor">
       <DashboardSideBar />
       <div className="grow flex-col h-full">
-        <DashboardHeader />
+        <Suspense fallback={<div>loading</div>}>
+          <DashboardHeader />
+        </Suspense>
+
         <DashboardBody />
       </div>
     </div>
