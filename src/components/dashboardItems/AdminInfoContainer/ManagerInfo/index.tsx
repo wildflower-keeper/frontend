18ebc390c9216fba4bdfb8fb1a40bc739be5a6da
imfreeman1 <dkfrhkt@gmail.com>
2024-08-20 15:30:33 +0900

    책임자가 지정되지 않았을 때, 화면이 나오지 않는 문제 해결

--- src/components/dashboardItems/AdminInfoContainer/ManagerInfo/index.tsx
+++ src/components/dashboardItems/AdminInfoContainer/ManagerInfo/index.tsx
@@ -19,10 +19,20 @@ const ManagerInfo = ({ chiefOfficer, dutyOfficer }: ManagerInfoType) => {
     <div className="flex flex-col items-start gap-2">
       <div className="flex gap-2 min-w-64 justify-start">
         <span className="font-semibold">책임자</span>
-        <span>{chiefOfficer.name}</span>
-        <span className="text-fontWeak">
-          {formatPhoneNumber(chiefOfficer.phoneNumber)}
-        </span>
+        {chiefOfficer ? (
+          <>
+            <span>{chiefOfficer.name}</span>
+            <span className="text-fontWeak">
+              {formatPhoneNumber(chiefOfficer.phoneNumber)}
+            </span>
+          </>
+        ) : (
+          <>
+            <span className="text-fontWeak">
+              책임자가 지정되어있지 않습니다.
+            </span>
+          </>
+        )}
       </div>
       <div className="flex gap-2 min-w-64 justify-start">
         <span className="font-semibold">당직자</span>
