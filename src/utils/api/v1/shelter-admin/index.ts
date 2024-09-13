884ae905db8c1eff8f67584532a2d153f09a3ba3
imfreeman1 <dkfrhkt@gmail.com>
2024-09-13 14:28:03 +0900

    [FIX] 통신함수 수정
     - POST를 GET 통신으로 수정하였습니다.

--- src/utils/api/v1/shelter-admin/index.ts
+++ src/utils/api/v1/shelter-admin/index.ts
@@ -1,13 +1,12 @@
-import * as ROUTES from "./Routes.const"
-import { POST } from '../../axios';
+import * as ROUTES from "./Routes.const";
+import { GET, POST } from "../../axios";
 // Types
-import type { CurrentUserInfo } from './type';
+import type { CurrentUserInfo } from "./type";
 
 export function logout(): Promise<void> {
-    return POST({ url: ROUTES.LOGOUT });
+  return POST({ url: ROUTES.LOGOUT });
 }
 
 export function homelessPeopleCount(): Promise<CurrentUserInfo> {
-    return POST({ url: ROUTES.HOMELESS_PEOPLE_COUNT });
+  return GET({ url: ROUTES.HOMELESS_PEOPLE_COUNT });
 }
-
