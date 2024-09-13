b38def530a75b0142406ea3376863274000d9cbf
imfreeman1 <dkfrhkt@gmail.com>
2024-09-13 14:27:18 +0900

    [FIX]baseURL 수정, 통신함수 수정
     - 서버에 ssl이 적용되어있어 baseURL을 https로 변경하였습니다.
     - 통신함수 baseURL 적용되지 않는 문제 해결을 위해 axios.request로 통신하던 것을 customAxios.request로 통신하도록 변경하였습니다.

--- src/utils/api/axios.ts
+++ src/utils/api/axios.ts
@@ -1,40 +1,47 @@
-import axios, { AxiosError, AxiosRequestConfig, AxiosResponse, InternalAxiosRequestConfig } from "axios";
-import { Interceptor } from './__type__';
-import { V1_UNUSING_TOKEN_API } from './v1/common.const';
-import { getCookie } from '../cookie';
-import { isArray } from 'lodash';
+import axios, {
+  AxiosError,
+  AxiosRequestConfig,
+  AxiosResponse,
+  InternalAxiosRequestConfig,
+} from "axios";
+import { Interceptor } from "./__type__";
+import { V1_UNUSING_TOKEN_API } from "./v1/common.const";
+import { getCookie } from "../cookie";
+import { isArray } from "lodash";
 
 const customAxios = axios.create({
-  baseURL: "http://api.wildflower-gardening.com",
+  baseURL: "https://api.wildflower-gardening.com",
   headers: {
     accept: "*/*",
     "Content-Type": "application/json",
     // 창수
-
   },
 });
 
 const requestInterceptor: Interceptor<InternalAxiosRequestConfig> = {
-  onFulfilled: config => {
+  onFulfilled: (config) => {
     const { method, url } = config;
 
-    const unUseTokenCondition = V1_UNUSING_TOKEN_API.some(api => api.method === method && url?.includes(api.path))
+    const unUseTokenCondition = V1_UNUSING_TOKEN_API.some(
+      (api) => api.method === method && url?.includes(api.path),
+    );
 
-    if (!unUseTokenCondition) config.headers.set("auth-token", getCookie("authToken"));
+    if (!unUseTokenCondition)
+      config.headers.set("auth-token", getCookie("authToken"));
 
     return config;
   },
-  onRejected: error => {
+  onRejected: (error) => {
     return error;
   },
 };
 
-
 const responseInterceptor: Interceptor<AxiosResponse> = {
-  onFulfilled: config => {
+  onFulfilled: (config) => {
     if (config.status >= 400) {
       const code = config.data?.code || config.statusText;
-      const messageInConfig = config.data.message || config.data?.error?.message;
+      const messageInConfig =
+        config.data.message || config.data?.error?.message;
 
       const message =
         typeof messageInConfig === "string"
@@ -48,19 +55,29 @@ const responseInterceptor: Interceptor<AxiosResponse> = {
 
     return config;
   },
-  onRejected: error => {
+  onRejected: (error) => {
     return error;
   },
+};
 
-}
+customAxios.interceptors.request.use(
+  requestInterceptor.onFulfilled,
+  requestInterceptor.onRejected,
+);
+customAxios.interceptors.response.use(
+  responseInterceptor.onFulfilled,
+  responseInterceptor.onRejected,
+);
 
-customAxios.interceptors.request.use(requestInterceptor.onFulfilled, requestInterceptor.onRejected)
-customAxios.interceptors.response.use(responseInterceptor.onFulfilled, responseInterceptor.onRejected)
-
-export const GET = (option: AxiosRequestConfig<unknown>) => axios.request({ ...option, method: "GET" }).then(res => res.data)
-export const POST = (option: AxiosRequestConfig<unknown>) => axios.request({ ...option, method: "POST" }).then(res => res.data)
-export const DELETE = (option: AxiosRequestConfig<unknown>[]) => axios.request({ ...option, method: "DELETE" }).then(res => res.data)
-export const PUT = (option: AxiosRequestConfig<unknown>) => axios.request({ ...option, method: "PUT" }).then(res => res.data)
-export const PATCH = (option: AxiosRequestConfig<unknown>) => axios.request({ ...option, method: "PATCH" }).then(res => res.data)
+export const GET = (option: AxiosRequestConfig<unknown>) =>
+  customAxios.request({ ...option, method: "GET" }).then((res) => res.data);
+export const POST = (option: AxiosRequestConfig<unknown>) =>
+  customAxios.request({ ...option, method: "POST" }).then((res) => res.data);
+export const DELETE = (option: AxiosRequestConfig<unknown>[]) =>
+  customAxios.request({ ...option, method: "DELETE" }).then((res) => res.data);
+export const PUT = (option: AxiosRequestConfig<unknown>) =>
+  customAxios.request({ ...option, method: "PUT" }).then((res) => res.data);
+export const PATCH = (option: AxiosRequestConfig<unknown>) =>
+  customAxios.request({ ...option, method: "PATCH" }).then((res) => res.data);
 
 export default customAxios;
