37aa1fba6be803af0b398c23c2a8ec2fc527292c
imfreeman1 <dkfrhkt@gmail.com>
2024-09-13 18:04:16 +0900

    homelessPeopleList 통신함수 분리
     - 이용자 관리 내역 통신함수 분리
     - 사용되지 않는 import 삭제

--- src/components/dashboardItems/UserBoardContainer/index.tsx
+++ src/components/dashboardItems/UserBoardContainer/index.tsx
@@ -1,16 +1,17 @@
 "use client";
 
 import React, { useEffect, useState } from "react";
-import customAxios from "@/utils/api/axios";
 import { formatDateTime } from "@/utils/date/date";
-import { getCookie } from "@/utils/cookie";
 import totalPagesMaker from "@/utils/pagenation";
 import SearchInput, { filterValueType } from "./SearchInput";
 import SearchSelector, { filterType } from "./SearchSelector";
-import UserBoard, { userItemType } from "./UserBoard";
+import UserBoard from "./UserBoard";
 import PagenationButtonContainer from "./PagenationButtonContainer";
+import { homelessPeopleList } from "@/utils/api/v1/shelter-admin";
+//Type
+import type { UserItemType } from "@/utils/api/v1/shelter-admin/type";
 
-export type userItemListType = userItemType[];
+export type userItemListType = UserItemType[];
 
 type filterUserType = {
   filter: filterType;
@@ -28,19 +29,11 @@ const UserBoardContainer = () => {
   const [pageNumber, setPageNumber] = useState<number>(1);
   const [totalPages, setTotalPages] = useState<number[]>([]);
   const fetchUser = async (pageNum: number) => {
-    const url = `/api/v1/shelter-admin/homeless-people?filterType=${filterUser.filter}&filterValue=${filterUser.filterValue}&sleepoverTargetDate=${formatDateTime(new Date())}&pageNumber=${pageNum}&pageSize=6`;
-
-    const res = await customAxios({
-      url,
-      method: "GET",
-      headers: {
-        "auth-token": getCookie("authToken"),
-      },
+    const URL = `?filterType=${filterUser.filter}&filterValue=${filterUser.filterValue}&sleepoverTargetDate=${formatDateTime(new Date())}&pageNumber=${pageNum}&pageSize=6`;
+    homelessPeopleList(URL).then((res) => {
+      setUserItemList(res.items);
+      setTotalPages(totalPagesMaker(res.pagination.lastPageNumber));
     });
-    if (res.status === 200) {
-      setUserItemList([...res.data.items]);
-      setTotalPages(totalPagesMaker(res.data.pagination.lastPageNumber));
-    }
   };
   useEffect(() => {
     fetchUser(pageNumber);
--- src/utils/api/v1/shelter-admin/index.ts
+++ src/utils/api/v1/shelter-admin/index.ts
@@ -1,7 +1,7 @@
 import * as ROUTES from "./Routes.const";
 import { GET, POST } from "../../axios";
 // Types
-import type { CurrentUserInfo } from "./type";
+import type { CurrentUserInfo, HomelessPeopleListResponseType } from "./type";
 
 export function logout(): Promise<void> {
   return POST({ url: ROUTES.LOGOUT });
@@ -9,4 +9,11 @@ export function logout(): Promise<void> {
 
 export function homelessPeopleCount(): Promise<CurrentUserInfo> {
   return GET({ url: ROUTES.HOMELESS_PEOPLE_COUNT });
+}
+
+export function homelessPeopleList(
+  queryString: string,
+): Promise<HomelessPeopleListResponseType> {
+  const URL = ROUTES.HOMELESS_PEOPLE + queryString;
+  return GET({ url: URL });
 }
