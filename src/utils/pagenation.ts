c19155d178d836cfba4293daaf8e805a44ad123a
imfreeman1 <dkfrhkt@gmail.com>
2024-09-23 16:05:58 +0900

    [refactor] lodash를 이용하여 배열 생성
     - 페이지네이션 배열을 lodash의 range를 이용하여 생성하도록 수정

--- src/utils/pagenation.ts
+++ src/utils/pagenation.ts
@@ -1,3 +1,5 @@
+import _ from "lodash";
+
 const totalPagesMaker = ({
   currentPageNumber,
   lastPageNumber,
@@ -9,16 +11,11 @@ const totalPagesMaker = ({
 }) => {
   if (!lastPageNumber || !currentPageNumber) return null;
 
-  const result = [];
-
   const groupIndex = Math.floor((currentPageNumber - 1) / pageListSize);
   const startPoint = groupIndex * pageListSize + 1;
   const endPoint = Math.min((groupIndex + 1) * pageListSize, lastPageNumber);
 
-  for (let i = startPoint; i <= endPoint; i += 1) {
-    result.push(i);
-  }
-  return result;
+  return _.range(startPoint, endPoint);
 };
 
 export default totalPagesMaker;
