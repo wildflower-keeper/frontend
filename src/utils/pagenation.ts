cefe9194561bbecd8880a392a92805b28932f4fc
imfreeman1 <dkfrhkt@gmail.com>
2024-09-23 11:41:32 +0900

    [refactor] pagenation 컴포넌트 리팩터
     - 버튼 리스트를 페이지네이션 컴포넌트 내부에서 생성하도록 변경
     - pagenation util에서 현재 위치 기준으로 10개의 그룹을 생성하도록 변경

--- src/utils/pagenation.ts
+++ src/utils/pagenation.ts
@@ -1,6 +1,21 @@
-const totalPagesMaker = (lastPageNumber: number) => {
+const totalPagesMaker = ({
+  currentPageNumber,
+  lastPageNumber,
+  pageListSize,
+}: {
+  currentPageNumber: number | null;
+  lastPageNumber: number | null | undefined;
+  pageListSize: number;
+}) => {
+  if (!lastPageNumber || !currentPageNumber) return null;
+
   const result = [];
-  for (let i = 1; i <= lastPageNumber; i += 1) {
+
+  const groupIndex = Math.floor((currentPageNumber - 1) / pageListSize);
+  const startPoint = groupIndex * pageListSize + 1;
+  const endPoint = Math.min((groupIndex + 1) * pageListSize, lastPageNumber);
+
+  for (let i = startPoint; i <= endPoint; i += 1) {
     result.push(i);
   }
   return result;
