49c90d0c7428da340575cbcf1d2c58d6e4726ecf
dong1hyun <limd1238@naver.com>
2024-11-15 20:06:23 +0900

    옵션 객체로 전달

--- src/components/Chart/UserStatusChart/generateData.tsx
+++ src/components/Chart/UserStatusChart/generateData.tsx
@@ -1,44 +1,25 @@
 import { Point } from "@nivo/line";
 import range from "lodash/range"
 
-interface chartDataValueType {
+interface ChartDataValueType {
     x: string
     y: number
 }
 
-export interface chartDataType {
+export interface ChartDataType {
     id: string;
     color: string | undefined;
-    data: chartDataValueType[];
+    data: ChartDataValueType[];
 }
 
-interface generateChartDataType {
+interface GenerateChartDataType {
     id: string
     color: string | undefined
     countArray: number[] | undefined
 }
 
-interface generateMarginType {
-    top: number
-    right: number
-    bottom: number
-    left: number
-}
-
-interface generateAxisType {
-    size: number
-    padding: number
-    value: number[] | string[]
-}
-
-interface generateScaleType {
-    type: "point" | "linear"
-    min: number
-    max: number
-}
-
 export const generateCountData = (countArray: number[] | undefined) => {
-    const dataValueArray: chartDataValueType[] = [];
+    const dataValueArray: ChartDataValueType[] = [];
     (countArray || []).forEach((v, index) => {
         dataValueArray[index] = {
             "x": index + 1 + "일",
@@ -49,7 +30,7 @@ export const generateCountData = (countArray: number[] | undefined) => {
     return dataValueArray;
 }
 
-export const generateChartData = ({id, color, countArray}: generateChartDataType ) => ({
+export const generateChartData = ({ id, color, countArray }: GenerateChartDataType) => ({
     id,
     color,
     data: generateCountData(countArray)
@@ -57,40 +38,21 @@ export const generateChartData = ({id, color, countArray}: generateChartDataType
 
 export const AXIS_Y = range(10, 200, 10);
 
-export const generateMargin = ({top, right, bottom, left}: generateMarginType) => ({
-    top,
-    right,
-    bottom,
-    left
-})
-export const generateAxis = ({size, padding, value}: generateAxisType) => ({
-    tickSize: size,
-    tickPadding: padding,
-    tickValues: value
-});
-
-export const generateScale = ({type, min, max}: generateScaleType) => ({
-    type,
-    min,
-    max
-});
-
 
 export const generateStaticChartOption = () => {
     return {
-      margin: generateMargin({top: 10, right: 110, bottom: 50, left: 60}),
-      yScale: generateScale({type: "linear", min: 0, max: 50}),
-      axisLeft: {format: (value: number) => `${value}명`, ...generateAxis({size: 5, padding: 5, value: AXIS_Y})},
-      axisBottom: generateAxis({size: 5, padding: 5, value: ["1일", "8일", "15일", "22일", "30일"]}),
-      colors: (d: { color: string }) => d.color,
-      axisTop: null,
-      axisRight: null,
-      useMesh: true,
-      gridXValues: [],
-      gridYValues: AXIS_Y,
-      tooltip: ({ point }: { point: Point }) => (
-        <div>{point.data.yFormatted}명</div>
-      ),
+        margin: { top: 10, right: 110, bottom: 50, left: 60 },
+        yScale: { type: "linear" as const, min: 0, max: 50 },
+        axisLeft: { format: (value: number) => `${value}명`, size: 5, padding: 5, tickValues: AXIS_Y },
+        axisBottom: { size: 5, padding: 5, tickValues: ["1일", "8일", "15일", "22일", "30일"] },
+        colors: (d: { color: string }) => d.color,
+        axisTop: null,
+        axisRight: null,
+        useMesh: true,
+        gridXValues: [],
+        gridYValues: AXIS_Y,
+        tooltip: ({ point }: { point: Point }) => (
+            <div>{point.data.yFormatted}명</div>
+        ),
     };
-  };
-  
+};
