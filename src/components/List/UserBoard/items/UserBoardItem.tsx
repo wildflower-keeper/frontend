40947a6a8190c13f01387cd795ea521fde7d9703
dong1hyun <limd1238@naver.com>
2024-11-27 03:30:16 +0900

    외박 기간 설정

--- src/components/List/UserBoard/items/UserBoardItem.tsx
+++ src/components/List/UserBoard/items/UserBoardItem.tsx
@@ -20,15 +20,20 @@ const UserBoardItem = ({
   room,
   emergencyContact,
   reason,
-  targetDateSleepover
+  sleepoverStartDate,
+  sleepoverEndDate
 }: UserBoardItemType) => {
   const baseStyle = "h-fit my-auto text-center"
   const [isOpenStatusList, setIsOpenStatusList] = useState(false);
   const onStatusClick = () => {
     setIsOpenStatusList((prev) => !prev);
   }
+  const sleepoverPeriod = sleepoverStartDate && sleepoverEndDate ? `${sleepoverStartDate}~${sleepoverEndDate}` : "";
   return (
-    <div className="py-3 bg-white grid grid-cols-8 place-items-cente text-sm border-b border-solid border-neutral-200">
+    <div className="py-3 bg-white grid grid-cols-8 place-items-cente text-sm border-b border-solid border-neutral-200"
+      style={{
+        gridTemplateColumns: "1fr 1fr 1fr 1fr 2fr 2fr 2fr 2fr"
+      }}>
       <NumberOrCheckbox id={id} index={index} />
       <div className={`${baseStyle} relative`}>
         {
@@ -46,7 +51,7 @@ const UserBoardItem = ({
       <div className={`${baseStyle} truncate`}>{name}</div>
       <div className={`${baseStyle} truncate`}>{room}</div>
       <div className={`${baseStyle} min-w-[102px]`}>{formatPhoneNumber(phoneNumber)}</div>
-      <div className={baseStyle}>{targetDateSleepover}</div>
+      <div className={baseStyle}>{sleepoverPeriod}</div>
       <div className={baseStyle}>{reason}</div>
       <div className={baseStyle}>{emergencyContact}</div>
     </div>
