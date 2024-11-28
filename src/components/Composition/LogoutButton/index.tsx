8fa4b1be249f2c2c5e55d27eae9791b2cdab716a
dong1hyun <limd1238@naver.com>
2024-11-28 16:06:12 +0900

    로그아웃시 모든 캐시 삭제

--- src/components/Composition/LogoutButton/index.tsx
+++ src/components/Composition/LogoutButton/index.tsx
@@ -1,10 +1,11 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { logout } from "@/api/v1/shelter-admin";
 import { removeCookie } from "@/utils/cookie";
 import { LiaPowerOffSolid } from "react-icons/lia";
 import { useRouter } from "next/navigation";
 
 const LogoutButton = () => {
+    const queryClient = useQueryClient();
     const router = useRouter();
     const { mutate } = useMutation({
         mutationKey: logout.mutationKey(),
@@ -16,6 +17,7 @@ const LogoutButton = () => {
             onSuccess: () => {
                 removeCookie("authToken");
                 router.push('/auth');
+                queryClient.clear();
             },
             onError: (error) => {
                 return error;
@@ -25,7 +27,7 @@ const LogoutButton = () => {
 
     return (
         <button
-            className="flex flex-row items-center gap-2 basicSidbarbutton" onClick={handleLogout}>
+            className="flex flex-row items-center gap-2 basicSidebarbutton" onClick={handleLogout}>
             <LiaPowerOffSolid size={22} />
             <div className="text-md">
                 로그아웃
