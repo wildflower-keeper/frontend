7ac74f35be2bed9fce9b3b2f56c692e080420e55
dong1hyun <limd1238@naver.com>
2024-11-30 01:27:11 +0900

    폴더 이름 변경

--- src/components/Layout/NoticeContainer/index.tsx
+++ src/components/Layout/NoticeContainer/index.tsx
@@ -1,4 +1,4 @@
-import NoticeAddForm from "../NoticeAddForm";
+import AddNoticeForm from "../AddNoticeForm";
 import Notice from "./items/Notice";
 
 const NoticeContainer = () => {
@@ -6,7 +6,7 @@ const NoticeContainer = () => {
         <div>
             <div className="custom-page-name">공지사항</div>
             <div className="flex items-start justify-between gap-10 w-full">
-                <NoticeAddForm />
+                <AddNoticeForm />
                 <Notice />
             </div>
         </div>
